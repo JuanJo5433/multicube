f918d2c3ffb626509718e83e09743a6f3cd3da83
JuanJo5433 <juanjosegiraldo1001@gmail.com>
2024-09-29 21:51:02 -0500

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import Header from "./components/header";
 import product from "./assets/product.jpeg";
 
-
 const FeatureCard = ({ icon, title, description }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-6 m-2 w-full lg:w-[30%] border border-gray-200">
-      <div className="text-4xl text-[#F25430] mb-4">{icon}</div>
-      <h3 className="text-xl font-semibold text-[#031D29] mb-2">{title}</h3>
-      <p className="text-[#386873]">{description}</p>
+      <div className="text-4xl text-[#00E2D3] mb-4">{icon}</div>
+      <h3 className="text-xl font-semibold text-[#0D33B3] mb-2">{title}</h3>
+      <p className="text-[#005452]">{description}</p>
     </div>
   );
 };
@@ -16,16 +15,16 @@ const FeatureCard = ({ icon, title, description }) => {
 const ClientReviewCard = ({ name, title, comment, rating }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-6 m-2 w-full lg:w-[30%] border border-gray-200">
-      <h3 className="text-xl font-semibold text-[#031D29]">{name}</h3>
-      <p className="text-sm text-[#386873]">{title}</p>
-      <p className="text-[#031D29] mt-4">{comment}</p>
+      <h3 className="text-xl font-semibold text-[#0D33B3]">{name}</h3>
+      <p className="text-sm text-[#005452]">{title}</p>
+      <p className="text-[#0D33B3] mt-4">{comment}</p>
       <div className="flex mt-4">
         {[...Array(5)].map((_, index) => (
           <svg
             key={index}
             xmlns="http://www.w3.org/2000/svg"
             className={`h-5 w-5 ${
-              index < rating ? "text-[#F25430]" : "text-gray-300"
+              index < rating ? "text-[#00E2D3]" : "text-gray-300"
             }`}
             viewBox="0 0 20 20"
             fill="currentColor"
@@ -40,28 +39,26 @@ const ClientReviewCard = ({ name, title, comment, rating }) => {
 
 const Footer = () => {
   return (
-    <footer className="bg-[#031D29] text-white py-8 px-6 mt-36">
-      <div className=" bg-[#031D29] max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center">
-        <div className="bg-[#031D29] mb-8 lg:mb-0">
-          <h3 className="bg-[#031D29] text-2xl font-bold mb-4">Contáctanos</h3>
-          <p className="bg-[#031D29] mb-2">
-            <span className="font-medium mr-2 bg-[#031D29]">📞</span>+57 321 5717983
+    <footer className="bg-[#0D33B3] text-white py-8 px-6 mt-36">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center">
+        <div className="mb-8 lg:mb-0">
+          <h3 className="text-2xl font-bold mb-4">Contáctanos</h3>
+          <p className="mb-2">
+            <span className="font-medium mr-2">📞</span>+57 321 5717983
           </p>
-          <p className="bg-[#031D29] mb-2">
-            <span className="font-medium mr-2 bg-[#031D29]">📧</span>
+          <p className="mb-2">
+            <span className="font-medium mr-2">📧</span>
             parracallemanueljeronimo22@gmail.com
           </p>
-          <p className="bg-[#031D29]">
-            <span className="font-medium mr-2 bg-[#031D29]">📍</span>
+          <p>
+            <span className="font-medium mr-2">📍</span>
             Dosquebradas, Colombia
           </p>
         </div>
-        <div className="w-full lg:w-1/2 bg-[#031D29]">
-          <h3 className="text-2xl font-bold mb-4 bg-[#031D29]">
-            Envíanos un Mensaje
-          </h3>
+        <div className="w-full lg:w-1/2">
+          <h3 className="text-2xl font-bold mb-4">Envíanos un Mensaje</h3>
           <form>
-            <div className="grid gap-4 bg-[#031D29]">
+            <div className="grid gap-4">
               <input
                 type="text"
                 className="border border-gray-300 p-2 rounded w-full text-black"
@@ -74,7 +71,7 @@ const Footer = () => {
               />
               <button
                 type="submit"
-                className="bg-[#F25430] hover:bg-[#F26835] text-white py-2 px-4 rounded"
+                className="bg-[#00E2D3] hover:bg-[#00E7E3] text-white py-2 px-4 rounded"
               >
                 Enviar Mensaje
               </button>
@@ -82,7 +79,7 @@ const Footer = () => {
           </form>
         </div>
       </div>
-      <p className="text-center text-gray-400 mt-8 bg-[#031D29]">
+      <p className="text-center text-gray-400 mt-8">
         © 2024 MultiCube. Todos los derechos reservados.
       </p>
     </footer>
@@ -97,15 +94,14 @@ const App = () => {
         {/* Primera sección */}
         <section id="producto" className="flex flex-col lg:flex-row mt-36 mx-auto max-w-7xl p-6">
           <div className="flex flex-col justify-center px-6 lg:px-20 lg:w-1/2">
-            <h1 className="font-bold text-5xl md:text-7xl text-[#031D29]">
-              Cubo Multifuncional{" "}
+            <h1 className="font-bold text-5xl md:text-7xl text-[#0D33B3]">
+              Cubo Multifuncional
             </h1>
-            <span className="text-xl text-[#386873] mt-4">
-              Un producto revolucionario diseñado para mejorar la calidad de
-              vida en nuestros hogares.
+            <span className="text-xl text-[#005452] mt-4">
+              Un producto revolucionario diseñado para mejorar la calidad de vida en nuestros hogares.
             </span>
             <div className="mt-6 flex space-x-4">
-              <button className="bg-black text-white py-2 px-4 rounded-lg shadow hover:bg-gray-800 transition duration-200">
+              <button className="bg-[#00E2D3] text-white py-2 px-4 rounded-lg shadow hover:bg-[#00E7E3] transition duration-200">
                 Comprar Ahora
               </button>
             </div>
@@ -119,21 +115,22 @@ const App = () => {
           </div>
         </section>
         <div className="border-t border-gray-300 my-12"></div> {/* Divider */}
+        
         {/* Sección de características */}
         <section id="caracteristicas" className="mt-36 px-6">
-          <h2 className="text-3xl font-bold text-[#031D29]">
+          <h2 className="text-3xl font-bold text-[#0D33B3]">
             Características Principales
           </h2>
           <div className="flex flex-wrap mt-6 justify-between">
             <FeatureCard
               icon="⚡"
               title="Responsabilidad ambiental"
-              description="nos preocupamos por el medio ambiente y estamos comprometidos con prácticas comerciales sostenibles. Nos esforzamos por incorporar materiales y tecnologías respetuosas con el medio ambiente en el diseño y producción de nuestro proyecto. A su vez, buscamos fomentar el reciclaje adecuado de nuestro producto y minimizar cualquier impacto negativo en el entorno."
+              description="Nos preocupamos por el medio ambiente y estamos comprometidos con prácticas comerciales sostenibles. Nos esforzamos por incorporar materiales y tecnologías respetuosas con el medio ambiente en el diseño y producción de nuestro proyecto. A su vez, buscamos fomentar el reciclaje adecuado de nuestro producto y minimizar cualquier impacto negativo en el entorno."
             />
             <FeatureCard
               icon="👥"
               title="Comodidad"
-              description="Es nuestra caracteristica principal, ya que la vida de nuestros usuarios sea más fácil. Nos enfocamos por asegurar que nuestras herramientas sean intuitivas y fáciles de usar, simplificando las tareas cotidianas y brindando una experiencia sin complicaciones."
+              description="Es nuestra característica principal, ya que la vida de nuestros usuarios sea más fácil. Nos enfocamos por asegurar que nuestras herramientas sean intuitivas y fáciles de usar, simplificando las tareas cotidianas y brindando una experiencia sin complicaciones."
             />
             <FeatureCard
               icon="🏆"
@@ -142,13 +139,15 @@ const App = () => {
             />
           </div>
         </section>
+        
         <div id="testimonios" className="border-t border-gray-300 my-12"></div> {/* Divider */}
+        
         {/* Sección de comentarios de clientes */}
         <section className="mt-36 px-6">
-          <h2 className="text-3xl font-bold text-[#031D29]">
+          <h2 className="text-3xl font-bold text-[#0D33B3]">
             Lo que Dicen Nuestros Clientes
           </h2>
-          <p className="text-lg text-[#386873] mt-2">
+          <p className="text-lg text-[#005452] mt-2">
             Comentarios de clientes satisfechos...
           </p>
           <div className="flex flex-wrap mt-6 justify-between">
@@ -169,54 +168,15 @@ const App = () => {
             />
           </div>
         </section>
+        
         <div className="border-t border-gray-300 my-12"></div> {/* Divider */}
+        
         {/* Sección sobre nosotros */}
-        <section id="sobre-nosotros" className="mt-36 px-6  py-12">
-          <h2 className="text-3xl font-bold text-[#031D29] text-center">
-            Sobre Nosotros
-          </h2>
-          <div className="mt-8 flex flex-col lg:flex-row justify-between">
-            <div className="lg:w-2/3 mr-24 text-justify">
-             
-            
-              <div className="mt-8">
-                <h3 className="text-2xl font-bold text-[#031D29]">Visión</h3>
-                <p className="text-lg text-[#386873] leading-relaxed mt-2">
-                  Ser la solución innovadora y versátil para mejorar la calidad
-                  de vida de nuestros clientes, proporcionando un producto líder
-                  en tecnología que combine funcionalidad y estilo en un diseño
-                  compacto y moderno.
-                </p>
-              </div>
-              <div className="mt-8">
-                <h3 className="text-2xl font-bold text-[#031D29]">Misión</h3>
-                <p className="text-lg text-[#386873] leading-relaxed mt-2">
-                  Nuestra misión es ofrecer a nuestros clientes un cubo
-                  multifuncional que integre un ventilador, ambientador,
-                  pantalla LCD y reloj digital en un solo dispositivo, brindando
-                  una experiencia única de comodidad y bienestar en cualquier
-                  entorno. Nos comprometemos a utilizar tecnología de
-                  vanguardia, materiales de alta calidad y un diseño elegante
-                  para superar las expectativas de nuestros clientes y
-                  enriquecer su vida cotidiana. Nuestro objetivo es ser
-                  reconocidos como líderes en innovación, calidad y satisfacción
-                  del cliente en el mercado de productos para el hogar y la
-                  oficina.
-                </p>
-              </div>
-            </div>
-            <div className="lg:w-1/3 bg-white shadow-md rounded-lg p-6 border border-gray-300">
-              <h3 className="text-2xl font-bold text-[#031D29] mb-4">
-                Nuestro Equipo
-              </h3>
-              <ul className="list-disc list-inside">
-                <li className="text-lg text-[#386873]">Jean Carlos Bermúdez</li>
-                <li className="text-lg text-[#386873]">
-                  Manuel Jerónimo Parra
-                </li>
-              </ul>
-            </div>
-          </div>
+        <section id="sobre-nosotros" className="mt-36 px-6">
+          <h2 className="text-3xl font-bold text-[#0D33B3]">Sobre Nosotros</h2>
+          <p className="mt-4 text-[#005452]">
+            En MultiCube, nos dedicamos a innovar y mejorar la vida de las personas a través de productos multifuncionales que son prácticos, eficientes y estéticamente agradables. Nuestro objetivo es ofrecer soluciones que se adapten a las necesidades de nuestros clientes y les permitan disfrutar de su hogar de una manera única.
+          </p>
         </section>
       </main>
       <Footer />
